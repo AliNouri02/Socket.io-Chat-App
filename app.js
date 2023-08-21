87a23aa496e608720edf2a718a7b6b8efc19149e
AliNouri02 <afreelancer15@gmail.com>
2023-08-21 17:56:13 +0330

    Create Room and nameSpaces

--- app.js
+++ app.js
@@ -16,33 +16,61 @@ server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-let onlineUsers = {}
 
 // Setup websocket
 
-io.on("connection", (socket) => {
+let onlineUsers = {}
+
+//simple example of middelware
+
+// io.use((socket, next) => {
+//     const token = socket.handshake.auth.token;
+//     const id = 11021102
+//     if (token == undefined) {
+//         console.log("client is not conneting");
+//     } else if (token != id) {
+//         console.log("you cant login");
+//     }
+//     if (token === id)
+//         next()
+// })
+
+const chatNamespace = io.of("/chat");
+
+
+chatNamespace.on("connection", (socket) => {
+    // console.log(`User connected. ${socket.id}`);
 
     // Listening
-    socket.on("login", nickname => {
-        console.log(`${nickname} Connected.`);
-        onlineUsers[socket.id] = nickname
-        io.sockets.emit("online", onlineUsers)
-    })
 
-    socket.on("disconnect",() => {
-        const disconnectedUser = onlineUsers[socket.id];
-        if (disconnectedUser) {
-            console.log(`User '${disconnectedUser}' disconnected. ${socket.id}`);
-            delete onlineUsers[socket.id];
-            io.sockets.emit("online", onlineUsers);
-        }
+    socket.on("login", (data) => {
+        console.log(`${data.nickname} Connected.`);
+        socket.join(data.roomNumber);
+
+        onlineUsers[socket.id] = {
+            nickname: data.nickname,
+            roomNumber: data.roomNumber,
+        };
+        chatNamespace.emit("online", onlineUsers);
     });
 
-    socket.on("chat_message", data => {
-        io.sockets.emit("chat_message", data)
-    })
+    socket.on("disconnect", () => {
+        console.log(`User disconnected.`);
+        delete onlineUsers[socket.id];
+        chatNamespace.emit("online", onlineUsers);
+    });
 
-    socket.on("typing", data => {
-        socket.broadcast.emit("typing", data)
-    })
+    socket.on("chat message", (data) => {
+        chatNamespace.to(data.roomNumber).emit("chat message", data);
+    });
+
+    socket.on("typing", (data) => {
+        socket.broadcast.in(data.roomNumber).emit("typing", data);
+    });
+
+    socket.on("pvChat", (data) => {
+        console.log(`Private Chat Data: ${data}`);
+        console.log(data.to);
+        chatNamespace.to(data.to).emit("pvChat", data);
+    });
 });
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,78 +1,112 @@
-const socket = io();
+"use strict";
 
-//Query DOM
+var socket = io();
+var chatNamespace = io("/chat"); //Query DOM
+
 var messageInput = document.getElementById("messageInput"),
     chatForm = document.getElementById("chatForm"),
     chatBox = document.getElementById("chat-box"),
     feedback = document.getElementById("feedback"),
     onlineUsers = document.getElementById("online-users-list"),
-    chatContainer = document.getElementById("chatContainer");
+    chatContainer = document.getElementById("chatContainer"),
+    pvChatForm = document.getElementById("pvChatForm"),
+    pvMessageInput = document.getElementById("pvMessageInput"),
+    modalTitle = document.getElementById("modalTitle"),
+    pvChatMessage = document.getElementById("pvChatMessage");
+var nickname = localStorage.getItem("nickname"),
+    roomNumber = localStorage.getItem("chatroom");
+var socketId; // Emit Events
 
-
-const nickname = localStorage.getItem("nickname");
-
-//Emit Events
-
-socket.emit("login", nickname);
-
-chatForm.addEventListener("submit", (e) => {
+chatNamespace.emit("login", {
+    nickname: nickname,
+    roomNumber: roomNumber,
+});
+chatForm.addEventListener("submit", function (e) {
     e.preventDefault();
+
     if (messageInput.value) {
-        socket.emit("chat_message", {
+        chatNamespace.emit("chat message", {
             message: messageInput.value,
-            name: nickname
+            name: nickname,
+            roomNumber: roomNumber,
         });
         messageInput.value = "";
     }
 });
-
-messageInput.addEventListener("keypress", () => {
-    socket.emit("typing", { name: nickname })
-})
-
-//Listening
-socket.on("chat_message", (data) => {
-    feedback.innerHTML = "";
-    chatBox.innerHTML += `
-  <li class="alert alert-light">
-      <span
-          class="text-dark font-weight-normal"
-          style="font-size: 13pt"
-          >${data.name}</span
-      >
-      <span
-          class="
-              text-muted
-              font-italic font-weight-light
-              m-2
-          "
-          style="font-size: 9pt"
-          >ساعت 12:00</span
-      >
-      <p
-          class="alert alert-info mt-2"
-          style="font-family: persian01"
-      >
-      ${data.message}
-      </p>
-  </li>`;
-    chatContainer.scrollTop = chatContainer.scrollHeight - chatContainer.clientHeight;
+messageInput.addEventListener("keypress", function () {
+    chatNamespace.emit("typing", {
+        name: nickname,
+        roomNumber: roomNumber,
+    });
 });
+pvChatForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    chatNamespace.emit("pvChat", {
+        message: pvMessageInput.value,
+        name: nickname,
+        to: socketId,
+        from: chatNamespace.id,
+    });
+    $("#pvChat").modal("hide");
+    pvMessageInput.value = "";
+}); // Listening
 
-socket.on("typing", (data) => {
-    feedback.innerHTML = `<p class="alert alert-warning w-25"><em>${data.name} در حال نوشتن است ... </em></p>`;
-});
-
-socket.on("online", data => {
+chatNamespace.on("online", function (users) {
     onlineUsers.innerHTML = "";
-    Object.values(data).forEach(value => {
-        onlineUsers.innerHTML += `
-        <li class="alert alert-light p-1 mx-2">
-            ${value}
-            <span class="badge badge-success">آنلاین</span>
-        </li>
-    `;
-    })
-})
 
+    for (var _socketId in users) {
+        if (roomNumber === users[_socketId].roomNumber) {
+            onlineUsers.innerHTML +=
+                '\n            <li >\n                <button type="button" class="btn btn-light mx-2 p-2" data-toggle="modal" data-target="#pvChat" data-id='
+                    .concat(_socketId, " data-client=")
+                    .concat(users[_socketId].nickname, "\n                ")
+                    .concat(
+                        users[_socketId].nickname === nickname
+                            ? "disabled"
+                            : "",
+                        ">\n                "
+                    )
+                    .concat(
+                        users[_socketId].nickname,
+                        '\n                    <span class="badge badge-success"> </span>\n                </button>\n            </li>\n        '
+                    );
+        }
+    }
+});
+chatNamespace.on("chat message", function (data) {
+    feedback.innerHTML = "";
+    chatBox.innerHTML +=
+        '\n                        <li class="alert alert-light">\n                            <span\n                                class="text-dark font-weight-normal"\n                                style="font-size: 13pt"\n                                >'
+            .concat(
+                data.name,
+                '</span\n                            >\n                            <span\n                                class="\n                                    text-muted\n                                    font-italic font-weight-light\n                                    m-2\n                                "\n                                style="font-size: 9pt"\n                                >\u0633\u0627\u0639\u062A 12:00</span\n                            >\n                            <p\n                                class="alert alert-info mt-2"\n                                style="font-family: persian01"\n                            >\n                            '
+            )
+            .concat(
+                data.message,
+                "\n                            </p>\n                        </li>"
+            );
+    chatContainer.scrollTop =
+        chatContainer.scrollHeight - chatContainer.clientHeight;
+});
+chatNamespace.on("typing", function (data) {
+    if (roomNumber === data.roomNumber)
+        feedback.innerHTML = '<p class="alert alert-warning w-25"><em>'.concat(
+            data.name,
+            " \u062F\u0631 \u062D\u0627\u0644 \u0646\u0648\u0634\u062A\u0646 \u0627\u0633\u062A ... </em></p>"
+        );
+});
+chatNamespace.on("pvChat", function (data) {
+    $("#pvChat").modal("show");
+    socketId = data.from;
+    modalTitle.innerHTML = "دریافت پیام از طرف : " + data.name;
+    pvChatMessage.style.display = "block";
+    pvChatMessage.innerHTML = data.name + " : " + data.message;
+}); //JQuery
 
+$("#pvChat").on("show.bs.modal", function (e) {
+    var button = $(e.relatedTarget);
+    var user = button.data("client");
+    socketId = button.data("id");
+    modalTitle.innerHTML = "ارسال پیام شخصی به :" + user;
+    pvChatMessage.style.display = "none";
+});
